ee8cb3b8d4c440b4111368758bcc81c636a4a73c
tranhatam-collab <tranhatam@gmail.com>
2026-03-17 22:47:32 +0700

    Add ESLint configuration file .eslintrc.cjs

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2022: true
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {}
+}
